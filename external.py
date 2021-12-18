b31a05302421cbabfb739072caad84f244fcdd87
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2021-12-18 12:45:21 -0600

    setting up alembic, models, and first migration

---
+++ external.py
@@ -0,0 +1,37 @@
+import logging
+from contextlib import contextmanager
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class Database:
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            instance = super(Database, cls).__new__(cls)
+            session_factory, engine = cls._build_session_factory()
+            instance.session_factory = session_factory
+            instance.engine = engine
+            cls._instance = instance
+
+        return getattr(cls, '_instance')
+
+    @contextmanager
+    def get_new_session(self):
+        session = self.session_factory()
+        try:
+            yield session
+            session.commit()
+        finally:
+            session.close()
+
+    @classmethod
+    def _build_session_factory(cls) -> sessionmaker:
+        db_user = os.environ.get('DB_USERNAME')
+        db_password = os.environ.get('DB_PASSWORD')
+        if not (db_user and db_password):
+            raise Exception('Database credentials not set')
+
+        engine = create_engine(f'postgresql://{db_user}:{db_password}@localhost:8967/postgres')
+        return sessionmaker(bind=engine, expire_on_commit=False), engine
---
+++ models.py
@@ -0,0 +1,31 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+BaseModel = declarative_base()
+
+
+class Ingredient(BaseModel):
+    __tablename__ = 'ingredients'
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(200))
+
+
+class Recipe(BaseModel):
+    __tablename__ = 'recipes'
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(200))
+
+
+class Measurement(BaseModel):
+    __tablename__ = 'measurements'
+    id = sa.Column(sa.Integer, primary_key=True)
+    amount = sa.Column(sa.Float)
+    ingredient_id = sa.Column(sa.Integer, sa.ForeignKey(Ingredient.id))
+    recipe_id = sa.Column(sa.Integer, sa.ForeignKey(Recipe.id))
+
+
+class Step(BaseModel):
+    __tablename__ = 'steps'
+    id = sa.Column(sa.Integer, primary_key=True)
+    number = sa.Column(sa.SmallInteger)
+    description = sa.Column(sa.String(2000))
